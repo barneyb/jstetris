bd4a887379e29d5a060cb1cb2f01b73e4606da58
barneyb <bboisvert@gmail.com>
2016-07-17 13:34:49 -0700

    moved Model into it's own file

--- script.js
+++ script.js
@@ -16,37 +16,6 @@ function randBool() {
     return randN(2) == 0;
 }
 
-function Model() {
-    this.state = STATE.NOT_STARTED;
-    this.lineCount = 0;
-    this.activePiece = null;
-    this.interval = null;
-    this.board = [];
-    this.initializeBoard();
-}
-Model.prototype.initializeBoard = function() {
-    for (var r = 0; r < ROWS; r++) {
-        this.board[r] = [];
-        for (var c = 0; c < COLS; c++) {
-            this.board[r][c] = BLACK;
-        }
-    }
-};
-Model.prototype.isPieceActive = function() {
-    return this.activePiece != null;
-};
-Model.prototype.isCellEmpty = function(r, c) {
-    return this.board[r][c] == BLACK;
-};
-Model.prototype.getCellColor = function(r, c) {
-    if (this.state == STATE.PAUSED) {
-        return BLACK;
-    }
-    if (this.activePiece != null && this.activePiece.isAt(r, c)) {
-        return this.activePiece.color;
-    }
-    return this.board[r][c];
-};
 model = new Model();
 ui = {
     lineCount: document.getElementById("lineCount"),
