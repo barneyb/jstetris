94b25400b18529942e7bcdd9ecf559269ce4cf3e
Barney Boisvert <bboisvert@gmail.com>
2016-07-26 12:58:41 -0700

    Piece.isAt with a loop bonus assignment

--- assignment.js
+++ assignment.js
@@ -15,6 +15,10 @@
  * for EVERY assignment is to uncomment the function, and refresh to see it
  * break. Then you can start filling in the function.
  *
+ * While you're working, if you just comment out the whole function again, the
+ * game will go back to it's original state. So you can always 'undo' your
+ * changes by commenting out your function, without having to delete it.
+ *
  * It will also be helpful to keep the developer tools open so you can see the
  * console. That way you can see if there are any errors, but more importantly,
  * you can use the `console.log` function to print out messages and/or variable
@@ -222,11 +226,12 @@ function labelLineCount(lineCount) {
  * "this.layout[3]" will be the column coordinate of the second block, and
  * "this.layout[6]" will be the row coordinate of the fourth block.
  *
- * Note: when you uncomment and refresh, both the piece preview and the current
- * active piece will stop showing up. Like the random boolean for centering,
- * this is due to the "false-y" nature of 'undefined', since isAt also returns
- * a boolean value. If you wait a few seconds, once the pieces land at the
- * bottom of the board they WILL show up.
+ * Note: when you uncomment and refresh, both the piece preview and the active
+ * piece will stop showing up. Like the random boolean for centering, this is
+ * due to the "false-y" nature of 'undefined', since isAt also returns a boolean
+ * value. If you wait a few seconds, once the pieces land at the bottom of the
+ * board they WILL show up. Or you can use the down arrow to drop the active
+ * piece to the bottom immediately.
  */
 
 /**
@@ -244,6 +249,47 @@ Piece.prototype.isAt = function isAt(r, c) {
         console.log("isAt(" + r + ", " + c + ")", this.layout);
     }
     // end "getting started" if statement
+    return (this.layout[0] == r && this.layout[1] == c) ||
+        (this.layout[2] == r && this.layout[3] == c) ||
+        (this.layout[4] == r && this.layout[5] == c) ||
+        (this.layout[6] == r && this.layout[7] == c);
+};
+
+/*
+ * Bonus Assignment!
+ *
+ * In the last assignment, you had to implement the Piece.isAt method. The
+ * instructions told you to just use indexes to get the coordinates out of the
+ * 'layout' array, because it's well-known that there are always exactly four
+ * blocks making up a given piece. However, there's no reason that Piece should
+ * necessarily assume Tetris. In fact, the entire game makes no assumptions
+ * about the size of the board or the size (or shape) of the pieces. Excepting
+ * that new isAt method you wrote. :)
+ *
+ * This assignment is to reimplement isAt using a loop, so that it will
+ * gracefully handle Pieces with any number of blocks in the layout array. Don't
+ * go change last assignment though, start over below. There isn't a helper
+ * console.log this time though, just a blank function.
+ *
+ * Also, because JavaScript methods are added with the assignment operator, that
+ * means you can change them by simply assigning a new function. That's what
+ * will happening below when you uncomment. So even though your function from
+ * the last assignment is still being set as the Piece.isAt method, this
+ * assignment's function is replacing it, just as if you were to do this:
+ *
+ *    var x = 4; // set the value
+ *    x = 5;     // replace it with another
+ */
+
+/**
+ * I return whether any of the piece's four blocks are at the passed row/column
+ * coordinates.
+ *
+ * @param r The row of the block in question
+ * @param c The column of the block in question
+ * @returns {boolean} Whether the piece is at the requested row and column.
+ */
+Piece.prototype.isAt = function isAt(r, c) {
     for (var i = 0; i < this.layout.length; i += 2) {
         if (this.layout[i] == r && this.layout[i + 1] == c) {
             return true;
